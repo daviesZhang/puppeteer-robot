f3f9c6cb797bc72f43cf4eaf63acbb75db8bbaf2
davies <sychopian@gmail.com>
2022-06-10 08:58:23 +0800

    initial commit

---
+++ apps/puppeteer-robot-server/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'puppeteer-robot-server',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/puppeteer-robot-server',
+};
